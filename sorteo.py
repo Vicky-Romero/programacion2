89e05bcdf12f6119bbc5cc7bd2a554b8eec691fb
Vicky-Romero <anamarci51@gmail.com>
2024-08-20 08:42:02 -0300

    Add files via upload

---
+++ sorteo.py
@@ -0,0 +1,15 @@
+import random
+estudiantes=[]
+estaux=""
+cantest=0
+orden=[]
+
+cantest=input("Ingrese la cantidad de estudiantes que desea tener..:")
+
+for i in range(len(cantest)):
+    estax=input("ingrese el nombre del alumno..:")
+    estudiantes.append(estaux)
+    
+    
+print(estudiantes)
+    
